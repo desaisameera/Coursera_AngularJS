df01db605ffc3b025c3d26294d0e3fb5b6533942
Sameera Desai <ssd9035@rit.edu>
2017-01-12 22:27:06 -0500

    Created the app for the assignment
    Used ng-model to read the data entered; ng-click to invoke the function
    when the button is clicked and injected the $scope into the controller.

--- Assignment1/assignment1/assignment1-starter-code/app.js
+++ Assignment1/assignment1/assignment1-starter-code/app.js
@@ -1,7 +1,29 @@
 (function(){
   'use strict';
   angular.module('LunchCheck',[])
-  .controller('LunchCheckController',function($scope){
+  .controller('LunchCheckController', LunchCheckController);
 
-  });
+    LunchCheckController.$inject = ['$scope'];
+
+    function LunchCheckController($scope) {
+      $scope.items  = "";
+      $scope.message = "";
+      $scope.checkIfTooMuch = function(){
+        if($scope.items) {
+          var itemsEntered = $scope.items.split(',');
+          if(itemsEntered.length < 1) {
+            $scope.message = "Please enter data first.";
+          }
+          else if(itemsEntered.length <= 3) {
+            $scope.message = "Enjoy!"
+          }
+          else if(itemsEntered.length > 3) {
+            $scope.message = "Too much";
+          }
+        }
+        else {
+          $scope.message = "Please enter data first";
+        }
+      };
+    }
 })();
